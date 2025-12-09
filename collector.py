7399683ee73cafae612a42860c05d6f7560e15c0
pooriaredorg <165016405+pooriaredorg@users.noreply.github.com>
2025-12-10 00:14:06 +0330

    Update collector.py

--- collector.py
+++ collector.py
@@ -39,7 +39,7 @@ JAVID_NAMAN: List[str] = [
 SUB_LINKS: List[str] = [
     "https://raw.githubusercontent.com/liketolivefree/kobabi/main/sub.txt",
     "",
-    "https://farsonline24.ir",
+    "http://sabapardaziran.ir/download/sub?target=V2Ray",
     "https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/main/vless.html"
 ]
 
